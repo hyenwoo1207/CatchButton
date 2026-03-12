2d67bb9c1547a0b3f752fcabacf54b6415532ed3
현우 강 <hyenwoo1207@gmail.com>
2026-03-12 12:17:39 +0900

    버튼이 창 밖으로 나가지 않게 수정

--- Form1.cs
+++ Form1.cs
@@ -13,19 +13,15 @@ namespace CatchButton1
 
         private void RunningButton1(object sender, EventArgs e)
         {
-
             // 1. 난수생성기준비
             Random rd = new Random();
             // 2. 가용영역계산(버튼이폼테두리에걸리지않게보호)
             // ClientSize는타이틀바와테두리를제외한실제흰도화지영역임
-            int maxX = this.ClientSize.Width;
-            int maxY = this.ClientSize.Height;
-            // 3. 랜덤좌표추출(버튼이 폼에서 완전히 보이도록 좌표 범위 조정)
-            // 버튼의 우측/하단이 폼을 벗어나지 않도록 최대 좌표를 버튼 크기만큼 줄임
-            int boundX = Math.Max(1, maxX - RunningButton.Width + 1);
-            int boundY = Math.Max(1, maxY - RunningButton.Height + 1);
-            int nextX = rd.Next(0, boundX);
-            int nextY = rd.Next(0, boundY);
+            int availX = this.ClientSize.Width - RunningButton.Width;
+            int availY = this.ClientSize.Height - RunningButton.Height;
+            // 3. 랜덤좌표추출(0 ~ 최대가용치사이). 버튼 크기를 고려해서 폼을 벗어나지 않도록 함
+            int nextX = availX > 0 ? rd.Next(0, availX + 1) : 0;
+            int nextY = availY > 0 ? rd.Next(0, availY + 1) : 0;
             // 4. 위치할당(새로운Point 객체생성)
             RunningButton.Location = new Point(nextX, nextY);
             // 5. 시각적피드백(폼제목표시줄에좌표출력)
